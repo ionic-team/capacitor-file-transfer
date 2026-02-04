d0e43ccf611bd54b507953af634ee1d354203eac
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2026-02-04 10:29:00 +0000

    fix(ios): http body in error and send progress only when success (#59)
    Cherry-picked for Capacitor 7
    
    * chore: Minor changes in app error display
    
    * chore: Add options in example app for http error
    
    * fix(ios): http body in error and send progress only when success
    
    Via native library update:
    
    References:
    
    - https://github.com/ionic-team/ion-ios-filetransfer/pull/13
    - https://outsystemsrd.atlassian.net/browse/RMET-4893

--- packages/capacitor-plugin/Package.swift
+++ packages/capacitor-plugin/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
-        .package(url: "https://github.com/ionic-team/ion-ios-filetransfer.git", from: "1.0.1")
+        .package(url: "https://github.com/ionic-team/ion-ios-filetransfer.git", from: "1.0.2")
     ],
     targets: [
         .target(
