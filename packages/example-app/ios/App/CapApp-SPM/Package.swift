63878f6b2a0989fbcd5a42f202dbb6f651f51e78
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-03-03 11:49:16 +0000

    feat: Replace CocoaPods iOS app with SPM

---
+++ packages/example-app/ios/App/CapApp-SPM/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
+let package = Package(
+    name: "CapApp-SPM",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapApp-SPM",
+            targets: ["CapApp-SPM"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.1.0"),
+        .package(name: "CapacitorFileTransfer", path: "../../../../capacitor-plugin"),
+        .package(name: "CapacitorFileViewer", path: "../../../node_modules/@capacitor/file-viewer"),
+        .package(name: "CapacitorFilesystem", path: "../../../node_modules/@capacitor/filesystem")
+    ],
+    targets: [
+        .target(
+            name: "CapApp-SPM",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorFileTransfer", package: "CapacitorFileTransfer"),
+                .product(name: "CapacitorFileViewer", package: "CapacitorFileViewer"),
+                .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem")
+            ]
+        )
+    ]
+)
